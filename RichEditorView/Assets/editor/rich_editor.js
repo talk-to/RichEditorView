504b892ab8d786728143e29453923f68be9ca013
Caesar Wirth <cjwirth@gmail.com>
2015-10-16 11:45:37 +0900

    Added loadScript function to load JS scripts in a way that we can debug them easier

--- RichEditorView/Assets/editor/rich_editor.js
+++ RichEditorView/Assets/editor/rich_editor.js
@@ -17,6 +17,18 @@
 
 var RE = {};
 
+function loadScript(url, callback) {
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    head.appendChild(script);
+}
+
 window.onload = function() {
     RE.callback("ready");
 };
