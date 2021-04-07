fd5fea083a15f14a95d1f9e06e3b1a48346be4aa
Fi4m <vedantma@flock.com>
2021-04-07 15:09:29 +0530

    Switch case indentation fixes

--- RichEditorView/Classes/RichEditorOptionItem.swift
+++ RichEditorView/Classes/RichEditorOptionItem.swift
@@ -90,27 +90,27 @@ public enum RichEditorDefaultOption: RichEditorOption {
     public var image: UIImage? {
         var name = ""
         switch self {
-            case .clear: name = "clear"
-            case .undo: name = "undo"
-            case .redo: name = "redo"
-            case .bold: name = "bold"
-            case .italic: name = "italic"
-            case .subscript: name = "subscript"
-            case .superscript: name = "superscript"
-            case .strike: name = "strikethrough"
-            case .underline: name = "underline"
-            case .textColor: name = "text_color"
-            case .textBackgroundColor: name = "bg_color"
-            case .header(let h): name = "h\(h)"
-            case .indent: name = "indent"
-            case .outdent: name = "outdent"
-            case .orderedList: name = "ordered_list"
-            case .unorderedList: name = "unordered_list"
-            case .alignLeft: name = "justify_left"
-            case .alignCenter: name = "justify_center"
-            case .alignRight: name = "justify_right"
-            case .image: name = "insert_image"
-            case .link: name = "insert_link"
+        case .clear: name = "clear"
+        case .undo: name = "undo"
+        case .redo: name = "redo"
+        case .bold: name = "bold"
+        case .italic: name = "italic"
+        case .subscript: name = "subscript"
+        case .superscript: name = "superscript"
+        case .strike: name = "strikethrough"
+        case .underline: name = "underline"
+        case .textColor: name = "text_color"
+        case .textBackgroundColor: name = "bg_color"
+        case .header(let h): name = "h\(h)"
+        case .indent: name = "indent"
+        case .outdent: name = "outdent"
+        case .orderedList: name = "ordered_list"
+        case .unorderedList: name = "unordered_list"
+        case .alignLeft: name = "justify_left"
+        case .alignCenter: name = "justify_center"
+        case .alignRight: name = "justify_right"
+        case .image: name = "insert_image"
+        case .link: name = "insert_link"
         }
 
         let bundle = Bundle(for: RichEditorToolbar.self)
@@ -119,53 +119,53 @@ public enum RichEditorDefaultOption: RichEditorOption {
 
     public var title: String {
         switch self {
-            case .clear: return NSLocalizedString("Clear", comment: "")
-            case .undo: return NSLocalizedString("Undo", comment: "")
-            case .redo: return NSLocalizedString("Redo", comment: "")
-            case .bold: return NSLocalizedString("Bold", comment: "")
-            case .italic: return NSLocalizedString("Italic", comment: "")
-            case .subscript: return NSLocalizedString("Sub", comment: "")
-            case .superscript: return NSLocalizedString("Super", comment: "")
-            case .strike: return NSLocalizedString("Strike", comment: "")
-            case .underline: return NSLocalizedString("Underline", comment: "")
-            case .textColor: return NSLocalizedString("Color", comment: "")
-            case .textBackgroundColor: return NSLocalizedString("BG Color", comment: "")
-            case .header(let h): return NSLocalizedString("H\(h)", comment: "")
-            case .indent: return NSLocalizedString("Indent", comment: "")
-            case .outdent: return NSLocalizedString("Outdent", comment: "")
-            case .orderedList: return NSLocalizedString("Ordered List", comment: "")
-            case .unorderedList: return NSLocalizedString("Unordered List", comment: "")
-            case .alignLeft: return NSLocalizedString("Left", comment: "")
-            case .alignCenter: return NSLocalizedString("Center", comment: "")
-            case .alignRight: return NSLocalizedString("Right", comment: "")
-            case .image: return NSLocalizedString("Image", comment: "")
-            case .link: return NSLocalizedString("Link", comment: "")
+        case .clear: return NSLocalizedString("Clear", comment: "")
+        case .undo: return NSLocalizedString("Undo", comment: "")
+        case .redo: return NSLocalizedString("Redo", comment: "")
+        case .bold: return NSLocalizedString("Bold", comment: "")
+        case .italic: return NSLocalizedString("Italic", comment: "")
+        case .subscript: return NSLocalizedString("Sub", comment: "")
+        case .superscript: return NSLocalizedString("Super", comment: "")
+        case .strike: return NSLocalizedString("Strike", comment: "")
+        case .underline: return NSLocalizedString("Underline", comment: "")
+        case .textColor: return NSLocalizedString("Color", comment: "")
+        case .textBackgroundColor: return NSLocalizedString("BG Color", comment: "")
+        case .header(let h): return NSLocalizedString("H\(h)", comment: "")
+        case .indent: return NSLocalizedString("Indent", comment: "")
+        case .outdent: return NSLocalizedString("Outdent", comment: "")
+        case .orderedList: return NSLocalizedString("Ordered List", comment: "")
+        case .unorderedList: return NSLocalizedString("Unordered List", comment: "")
+        case .alignLeft: return NSLocalizedString("Left", comment: "")
+        case .alignCenter: return NSLocalizedString("Center", comment: "")
+        case .alignRight: return NSLocalizedString("Right", comment: "")
+        case .image: return NSLocalizedString("Image", comment: "")
+        case .link: return NSLocalizedString("Link", comment: "")
         }
     }
 
     public func action(_ toolbar: RichEditorToolbar) {
         switch self {
-            case .clear: toolbar.editor?.removeFormat()
-            case .undo: toolbar.editor?.undo()
-            case .redo: toolbar.editor?.redo()
-            case .bold: toolbar.editor?.bold()
-            case .italic: toolbar.editor?.italic()
-            case .subscript: toolbar.editor?.subscriptText()
-            case .superscript: toolbar.editor?.superscript()
-            case .strike: toolbar.editor?.strikethrough()
-            case .underline: toolbar.editor?.underline()
-            case .textColor: toolbar.delegate?.richEditorToolbarChangeTextColor?(toolbar)
-            case .textBackgroundColor: toolbar.delegate?.richEditorToolbarChangeBackgroundColor?(toolbar)
-            case .header(let h): toolbar.editor?.header(h)
-            case .indent: toolbar.editor?.indent()
-            case .outdent: toolbar.editor?.outdent()
-            case .orderedList: toolbar.editor?.orderedList()
-            case .unorderedList: toolbar.editor?.unorderedList()
-            case .alignLeft: toolbar.editor?.alignLeft()
-            case .alignCenter: toolbar.editor?.alignCenter()
-            case .alignRight: toolbar.editor?.alignRight()
-            case .image: toolbar.delegate?.richEditorToolbarInsertImage?(toolbar)
-            case .link: toolbar.delegate?.richEditorToolbarInsertLink?(toolbar)
+        case .clear: toolbar.editor?.removeFormat()
+        case .undo: toolbar.editor?.undo()
+        case .redo: toolbar.editor?.redo()
+        case .bold: toolbar.editor?.bold()
+        case .italic: toolbar.editor?.italic()
+        case .subscript: toolbar.editor?.subscriptText()
+        case .superscript: toolbar.editor?.superscript()
+        case .strike: toolbar.editor?.strikethrough()
+        case .underline: toolbar.editor?.underline()
+        case .textColor: toolbar.delegate?.richEditorToolbarChangeTextColor?(toolbar)
+        case .textBackgroundColor: toolbar.delegate?.richEditorToolbarChangeBackgroundColor?(toolbar)
+        case .header(let h): toolbar.editor?.header(h)
+        case .indent: toolbar.editor?.indent()
+        case .outdent: toolbar.editor?.outdent()
+        case .orderedList: toolbar.editor?.orderedList()
+        case .unorderedList: toolbar.editor?.unorderedList()
+        case .alignLeft: toolbar.editor?.alignLeft()
+        case .alignCenter: toolbar.editor?.alignCenter()
+        case .alignRight: toolbar.editor?.alignRight()
+        case .image: toolbar.delegate?.richEditorToolbarInsertImage?(toolbar)
+        case .link: toolbar.delegate?.richEditorToolbarInsertLink?(toolbar)
         }
     }
 }
