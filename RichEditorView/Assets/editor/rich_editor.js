deabc9c1b42d34d28a76a1a10c7060289752f188
Fi4m <vedantma@flock.com>
2021-04-09 15:18:44 +0530

    Revert "Fix - placeholder not visible during initialisation of composer"
    This reverts commit 19dbfd010f3710de689a308db80889fdaf34cfa1.

--- RichEditorView/Assets/editor/rich_editor.js
+++ RichEditorView/Assets/editor/rich_editor.js
@@ -259,10 +259,6 @@ RE.setJustifyRight = function() {
     document.execCommand('justifyRight', false, null);
 };
 
-RE.setLineHeight = function() {
-    RE.editor.style.lineHeight = 'normal';
-};
-
 RE.insertImage = function(url, alt) {
     var img = document.createElement('img');
     img.setAttribute("src", url);
