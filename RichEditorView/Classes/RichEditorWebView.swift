ce7171a075607fc958ef9332a4b2ad896a62ac91
Pratham Patel <prathamp@titan.email>
2026-08-14 16:01:40 +0530

    Add link menu support: open class, delegate, canPerformAction

--- RichEditorView/Classes/RichEditorWebView.swift
+++ RichEditorView/Classes/RichEditorWebView.swift
@@ -7,12 +7,40 @@
 
 import WebKit
 
-public class RichEditorWebView: WKWebView {
+open class RichEditorWebView: WKWebView {
 
     public var accessoryView: UIView?
+    public var linkMenuDelegate: RichEditorLinkMenuDelegate?
 
     public override var inputAccessoryView: UIView? {
         return accessoryView
     }
 
+    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(openLinkMenu) ||
+           action == #selector(editLinkMenu) ||
+           action == #selector(removeLinkMenu) {
+            return linkMenuDelegate?.isLinkAtCursor() ?? false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
+
+    @objc func openLinkMenu() {
+        linkMenuDelegate?.openLink()
+    }
+
+    @objc func editLinkMenu() {
+        linkMenuDelegate?.editLink()
+    }
+
+    @objc func removeLinkMenu() {
+        linkMenuDelegate?.removeLink()
+    }
+}
+
+public protocol RichEditorLinkMenuDelegate: AnyObject {
+    func isLinkAtCursor() -> Bool
+    func openLink()
+    func editLink()
+    func removeLink()
 }
